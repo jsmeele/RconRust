e1b0a061f1bafad9c44b4d7e9f681a446a6d772d
Jeffrey <jsmeele@users.noreply.github.com>
2020-01-08 19:33:08 +0100

    Initial app
    Simple Rcon Class with with a few functions:
    1) Connnect
    2) Close
    3) ReceiveMessage
    4) SendMessage

---
+++ rconRust.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 21:30:03 2020
+
+@author: Elementz
+"""
+
+import asyncio
+import websockets
+
+class RCONClient:
+
+    def __init__(self):
+        self.HOST           = 'xx.xx.xx.xx'     # IP/URL
+        self.PORT           = 28016             # Port to listen on
+        self.PASS           = 'password'        # Password
+        self.__BANLIST      = '{"identifier":1000,"message":"banlist"}'
+        self.__PLAYERLIST   = '{"identifier":2000,"message":"playerlist"}'
+        self.__URI          = 'ws://%s:%s/%s' % (self.HOST,self.PORT,self.PASS)
+        self.__CONNECTION   = None
+        self.__LOOP         = asyncio.get_event_loop()
+        self.__TASK         = None
+
+    async def connect(self):
+        # Connection to WebSocket
+        self.__CONNECTION = await websockets.connect(self.__URI, ping_interval=None)
+        
+        if self.__CONNECTION.open:
+            print('Connection established.')
+            # start the receiveMessage coroutines and coexist in Task wrappers
+            self.__TASK = asyncio.ensure_future(self.receiveMessage())
+            await self.__TASK
+            print('END')
+    
+    async def close(self):
+        self.__TASK.cancel()
+        # if not self.__TASK.cancelled():
+        #     self.__TASK.cancel()
+        # else:
+        #     self.__TASK = None
+            
+        await self.__CONNECTION.close()
+        print('Connection with server closed')
+    
+    async def receiveMessage(self):
+        # Receiving all server messages and handling them
+        while True:
+            try:
+                message = await self.__CONNECTION.recv()
+                print('Received message from server: ' + str(message))
+            except websockets.exceptions.ConnectionClosedError:
+                print('Connection Closed Error')
+                break
+
+    async def sendMessage(self, message):
+        # Sending message to webSocket server
+        print('Send Message.')
+        await self.__CONNECTION.send(message)
+    
+    def run(self):
+        # print(self.__LOOP)
+        if self.__LOOP.is_running():
+            asyncio.gather(self.connect())
+        else:
+            print('Event Loop Not Running')
+      
+        
+if __name__ == '__main__':
+    # Application object
+    client = RCONClient()
+    
+    # Start Application
+    client.run()
